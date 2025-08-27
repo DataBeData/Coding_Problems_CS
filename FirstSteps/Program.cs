4e6aaa4a0908f8a0cb047934388e6536505ce034
Ruben Van Zeveren <ruben.m.van.zeveren@gmail.com>
2025-08-27 16:20:22 +0200

    first commit

---
+++ FirstSteps/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Please enter a word: ");
+        string? userInput = Console.ReadLine();
+
+        Console.WriteLine($"You entered: {userInput}");
+    }
+}
+
