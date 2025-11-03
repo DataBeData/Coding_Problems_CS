ed80e7281e5d99f5e7691c038060d5f31ead2715
Ruben Van Zeveren <ruben.m.van.zeveren@gmail.com>
2025-11-03 20:06:14 +0100

    talking about classes

--- FirstSteps/Program.cs
+++ FirstSteps/Program.cs
@@ -1,41 +1,16 @@
 ﻿using System;
-using System.Linq.Expressions;
 
 class Program
 {
     static void Main()
 
     {
-      
-    }
-    
-    private static void EasyInterface()
-    {
-        Console.Write("Please enter a word: ");
-        string? userInput = Console.ReadLine();
-        Console.WriteLine($"You entered: {userInput}");
+        var sq = new Square();
+        sq.GetInput();
+        sq.MakingSquare();
+        
     }
 
-    private static void OddOrEven()
-    {
-        while (true)
-        {
+ }
 
-            Console.Write("Give a number: ");
-            string? userInput = Console.ReadLine();
-
-            if (int.TryParse(userInput, out int number))
-            {
-                Console.WriteLine($"You entered: {number}");
-                break;
-            }
-            else
-            {
-                Console.WriteLine("That's not a valid number!");
-            }
-         }
-    }
-
-    
-}
 
