2b14b48841445ea221dc5ea5679b4668816a5274
Ruben Van Zeveren <ruben.m.van.zeveren@gmail.com>
2025-12-24 15:33:20 +0100

    Commit Sankt Vith

--- FirstSteps/Program.cs
+++ FirstSteps/Program.cs
@@ -5,10 +5,6 @@ class Program
     static void Main()
 
     {
-        var sq = new Square();
-        sq.GetInput();
-        sq.MakingSquare();
-        
         Console.WriteLine(Calculator.Add(3, 4));      // prints 7
         Console.WriteLine(Calculator.Divide(10, 2));  // prints 5
     }
